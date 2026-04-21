a561b09f26545491fd2f62c293d637c844943c27
Dev Patel <devapatel09092006@gmail.com>
2026-04-21 09:02:59 +0530

    new

---
+++ Practicle-5/q1.py
@@ -0,0 +1,9 @@
+# Create and print a dictionary that contains keys a,b,c,d with their values 1,2,3 and 4 respectively using curly bracket syntax and ‘dict’ in built function. 
+
+d1 = {'a': 1, 'b' : 2, 'c' : 3, 'd' : 4}
+
+print("Dictionary using {}: ", d1)
+
+d2 = dict(a=1, b=2, c=3, d=4)
+
+print("Dictionary using dict(): ", d2)
